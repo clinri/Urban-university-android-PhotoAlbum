a4fcc71f04ba1ebf50e879c233d0cb6745e319eb
Andrei Borisov <clinri@ya.ru>
2024-12-30 23:37:54 +0300

    home work android 15 ImageView

--- app/src/main/java/ru/borisov/photoalbum/MainActivity.kt
+++ app/src/main/java/ru/borisov/photoalbum/MainActivity.kt
@@ -1,11 +1,21 @@
 package ru.borisov.photoalbum
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var startViewBTN: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startViewBTN = findViewById(R.id.startViewBTN)
+        startViewBTN.setOnClickListener {
+            startActivity(Intent(this, ImageActivity::class.java))
+            finish()
+            startViewBTN.isEnabled = false
+        }
     }
 }
